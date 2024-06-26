d3d3a205b914fd2ef32abe8a2ad9e1c03cdf75cc
DIdisuhardi <didisuhardi00@gmail.com>
2024-06-26 12:04:55 +0700

    create home screen navigation between Home and MovieDetail

---
+++ src/navigations/HomeStackNavigation.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+import MovieDetail from '../screens/MovieDetail';
+
+export type RootStackParamList = {
+  Home: undefined;
+  MovieDetail: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const HomeStackNavigation = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={Home} />
+    <Stack.Screen name="MovieDetail" component={MovieDetail} />
+  </Stack.Navigator>
+);
+
+export default HomeStackNavigation;
--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { View, Button,Text, StyleSheet} from 'react-native'
 
-export default function Home({navigation}:any): JSX.Element {
+export default function MovieDetail({navigation}:any): JSX.Element {
   return (
     <View style={styles.container}>
-      <Text>Movie Page</Text>
+      <Text>Movie Detail Page</Text>
       <Button
-        title="Go to MovieDetail" // Teks pada tombol
-        onPress={() => navigation.navigate('MovieDetail')} // Fungsi yang dipanggil saat tombol ditekan
+        title="Kembali" // Teks pada tombol
+        onPress={() => navigation.navigate('Home')} // Fungsi yang dipanggil saat tombol ditekan
       />
     </View>
   )
