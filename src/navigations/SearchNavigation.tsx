15032426b0c86f52f855c12f53121101a993d618
DIdisuhardi <didisuhardi00@gmail.com>
2024-06-26 20:12:50 +0700

    finalize of search feature

--- src/navigations/SearchNavigation.tsx
+++ src/navigations/SearchNavigation.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Search from '../screens/Search';
 import CategorySearchResult from '../screens/CategorySearchResult';
+import MovieDetail from '../screens/MovieDetail';
 
 const Stack = createNativeStackNavigator();
 
 const SearchNavigation = (): JSX.Element => {
   return (
 
-    <Stack.Navigator initialRouteName="Favorite">
+    <Stack.Navigator initialRouteName="Search">
       <Stack.Screen
         name="Search"
         component={Search }
@@ -18,6 +19,11 @@ const SearchNavigation = (): JSX.Element => {
         name="CategorySearchResult"
         component={CategorySearchResult}
         options={{ headerShown: true, title: 'Category Search Result' }}
+      />
+      <Stack.Screen
+        name="MovieDetail"
+        component={MovieDetail}
+        options={{ title: 'Movie Details' }}
       />
     </Stack.Navigator>
   );
